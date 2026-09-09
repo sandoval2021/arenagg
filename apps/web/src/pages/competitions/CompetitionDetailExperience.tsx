3c51014ff462dc320f39990f3113d38ecc8b47bb
sandoval2021 <78934835+sandoval2021@users.noreply.github.com>
2026-09-09 19:39:17 -0300

    feat(web): mount phase5 social and integrity center

--- apps/web/src/pages/competitions/CompetitionDetailExperience.tsx
+++ apps/web/src/pages/competitions/CompetitionDetailExperience.tsx
@@ -1,5 +1,6 @@
 import { KnockoutBracketDock } from '../../components/bracket/KnockoutBracketDock';
 import { CompetitionClipsPanel } from '../../components/clips/CompetitionClipsPanel';
+import { CompetitionPhaseFiveCenter } from '../../components/competition/CompetitionPhaseFiveCenter';
 import { CompetitionPrizePanel } from '../../components/competition/CompetitionPrizePanel';
 import { CompetitionMatchAutomationPanel } from '../../components/matches/CompetitionMatchAutomationPanel';
 import { CompetitionDetailPageLight } from './CompetitionDetailPageLight';
@@ -8,6 +9,7 @@ export function CompetitionDetailExperience() {
   return (
     <>
       <CompetitionDetailPageLight />
+      <CompetitionPhaseFiveCenter />
       <CompetitionPrizePanel />
       <CompetitionMatchAutomationPanel />
       <CompetitionClipsPanel />
