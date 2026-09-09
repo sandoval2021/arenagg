01345d730f8c5a336faaad63d6a9e1754bdfd6da
sandoval2021 <78934835+sandoval2021@users.noreply.github.com>
2026-09-09 12:53:32 -0300

    feat(web): mount bracket on competition details

---
+++ apps/web/src/pages/competitions/CompetitionDetailExperience.tsx
@@ -0,0 +1,11 @@
+import { KnockoutBracketDock } from '../../components/bracket/KnockoutBracketDock';
+import { CompetitionDetailPageLight } from './CompetitionDetailPageLight';
+
+export function CompetitionDetailExperience() {
+  return (
+    <>
+      <CompetitionDetailPageLight />
+      <KnockoutBracketDock />
+    </>
+  );
+}
