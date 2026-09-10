2046a1aa425109a88560dff964dfa0ffa7ca3026
sandoval2021 <78934835+sandoval2021@users.noreply.github.com>
2026-09-10 13:01:15 -0300

    feat(live): keep competition detail synchronized every 3 seconds

--- apps/web/src/pages/competitions/CompetitionDetailExperience.tsx
+++ apps/web/src/pages/competitions/CompetitionDetailExperience.tsx
@@ -1,3 +1,4 @@
+import { useParams } from 'react-router-dom';
 import { KnockoutBracketDock } from '../../components/bracket/KnockoutBracketDock';
 import { CompetitionClipsPanel } from '../../components/clips/CompetitionClipsPanel';
 import { CompetitionPhaseFiveCenter } from '../../components/competition/CompetitionPhaseFiveCenter';
@@ -5,9 +6,13 @@ import { CompetitionPrizePanel } from '../../components/competition/CompetitionP
 import { GroupStagePanel } from '../../components/competition/GroupStagePanel';
 import { CompetitionMatchAutomationPanel } from '../../components/matches/CompetitionMatchAutomationPanel';
 import { ReputationReviewPrompt } from '../../components/reputation/ReputationReviewPrompt';
+import { useCompetitionLivePolling } from '../../hooks/useCompetitionLivePolling';
 import { CompetitionDetailPageLight } from './CompetitionDetailPageLight';
 
 export function CompetitionDetailExperience() {
+  const { competitionId = '' } = useParams();
+  useCompetitionLivePolling(competitionId);
+
   return (
     <>
       <CompetitionDetailPageLight />
